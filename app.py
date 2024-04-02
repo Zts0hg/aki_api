ad717cb22787d0daf382ce61e4d1610727661df5
zhaobin <zhaobin_zhang@intsig.net>
2024-04-02 22:42:40 +0800

    feat: add root default response

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(example_blueprint, url_prefix='/grammar')
 
+
+@example_blueprint.route('/')
+def index():
+    return "Hello, World!"
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=6699, ssl_context=('xiaomingya.com.pem', 'xiaomingya.com.key'))
