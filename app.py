92c7977e073815e09b0602c3af49e908e3e77584
zhaobin <zhaobin_zhang@intsig.net>
2024-04-02 23:04:10 +0800

    feat: 备案需求的简单改动

--- app.py
+++ app.py
@@ -8,8 +8,26 @@ app.register_blueprint(example_blueprint, url_prefix='/grammar')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>小明呀</title>
+</head>
+
+<body>
+    <p>Hello, 这里是小明的个人博客.
+    </p>
+    <p> 目前博客仍在建设中....</p>
+
+</body>
+
+</html>
+    """
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6699, ssl_context=('xiaomingya.com.pem', 'xiaomingya.com.key'))
+    app.run(debug=True, host='0.0.0.0', port=6699)
