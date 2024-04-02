e0f643487f6dc34246b1466f9ac6dfd0498954b4
zhaobin <zhaobin_zhang@intsig.net>
2024-04-02 22:12:16 +0800

    feat: add ssl

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ from example_blueprint import example_blueprint
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(example_blueprint, url_prefix='/grammar')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6699, ssl_context=('xiaomingya.com.pem', 'xiaomingya.com.key'))
