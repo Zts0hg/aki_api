48d6f2f23473a34f6d033ffa93be09e74c5a797f
zhaobin <zhaobin_zhang@intsig.net>
2024-04-04 16:26:16 +0800

    feat: 增加N2第一课5个语法

--- example_blueprint.py
+++ example_blueprint.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify, request
 import pandas as pd
+from grammar_enumeration import grammars
 
 example_blueprint = Blueprint('example_blueprint', __name__)
-df = pd.read_json("grammar.json")
+# df = pd.read_json("grammar.json")
+df = pd.DataFrame(grammars)
 
 
 @example_blueprint.route('/')
@@ -15,7 +17,10 @@ def index():
             "data": [],
         })
 
-    df_res = df[df.content.str.contains(keyword) | df.hiragana.str.contains(keyword) | df.meaning.str.contains(keyword)]
+    if keyword == "akiakiaki":
+        df_res = df
+    else:
+        df_res = df[df.content.str.contains(keyword) | df.hiragana.str.contains(keyword) | df.meaning.str.contains(keyword)]
     return jsonify({
         "keyword": keyword,
         "data": [df.loc[index].to_dict() for index in df_res.index],
