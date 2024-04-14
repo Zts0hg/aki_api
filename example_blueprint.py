79889fe82f01e13c6df3304e5ef47e67573a1399
zhaobin <zhaobin_zhang@intsig.net>
2024-04-14 12:42:44 +0800

    feat: 新增N1-N5语法 blue book

--- example_blueprint.py
+++ example_blueprint.py
@@ -11,7 +11,10 @@ import re
 question_pattern = re.compile(r"[（(](?:[\da-n]\s*[~\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF]+\s*)+[)）]")
 example_blueprint = Blueprint('example_blueprint', __name__)
 last_audio_content = [""]
-df = df_grammars
+
+# df = df_grammars
+with open("grammars_N1-N5.json", "r", encoding="utf-8") as fp:
+    df = pd.DataFrame(json.loads(fp.read()))
 grammar_error_report_file_path = "/usr/local/grammar_error_report.json"
 
 if platform.system().casefold() != "windows" and not os.path.exists(grammar_error_report_file_path):
