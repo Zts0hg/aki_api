f46b3221dd83a0ae8ce1f1d659e213fa0ea369fb
zhaobin <zhaobin_zhang@intsig.net>
2024-07-26 04:29:52 +0800

    feat: 切换AI接口为gpt-4o

--- assistant.py
+++ assistant.py
@@ -1,34 +1,40 @@
-import openai
-import os
-import os
-from langchain.chat_models import AzureChatOpenAI
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.chains import ConversationChain
-from langchain import PromptTemplate, OpenAI, LLMChain
-from langchain.chat_models import AzureChatOpenAI
-from langchain.chains import SequentialChain
-from langchain.chat_models import AzureChatOpenAI
+from langchain import LLMChain, PromptTemplate
+from langchain_openai import AzureChatOpenAI
 
-deployment = "text_model"
-model = "gpt-35-turbo"
+# deployment = "text_model"
+# model = "gpt-35-turbo"
 
-openai_config = {
-    "openai_api_type": "azure",
-    # "openai_api_base": "https://ai-test-for-aki.openai.azure.com/",
-    "openai_api_base": "https://shibainu.openai.azure.com/",
-    "openai_api_version": "2023-07-01-preview",
-    "openai_api_key": os.environ.get("AKI_API_OPENAI_API_KEY"),
-}
-openai.api_type = openai_config["openai_api_type"]
-openai.api_base = openai_config["openai_api_base"]
-openai.api_version = openai_config["openai_api_version"]
-openai.api_key = openai_config["openai_api_key"]
+# openai_config = {
+#     "openai_api_type": "azure",
+#     # "openai_api_base": "https://ai-test-for-aki.openai.azure.com/",
+#     "openai_api_base": "https://shibainu.openai.azure.com/",
+#     "openai_api_version": "2023-07-01-preview",
+#     "openai_api_key": "f7d7d95881d440dda5f275c5574f04d0",
+#     # "openai_api_key": os.environ.get("AKI_API_OPENAI_API_KEY"),
+# }
+# openai.api_type = openai_config["openai_api_type"]
+# openai.api_base = openai_config["openai_api_base"]
+# openai.api_version = openai_config["openai_api_version"]
+# openai.api_key = openai_config["openai_api_key"]
+# print(openai_config)
 
 
 class Assistant:
-    llm = AzureChatOpenAI(deployment_name=deployment, model_name=model, temperature=0, max_tokens=2000, **openai_config)
+    llm = AzureChatOpenAI(
+        azure_deployment="gpt-4o",
+        api_version="2024-02-15-preview",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
     translating_prompt_template = """translate "{content}" into {target_language}:"""
-    translating_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(translating_prompt_template), output_key="translated")
+    translating_chain = LLMChain(
+        llm=llm,
+        prompt=PromptTemplate.from_template(translating_prompt_template),
+        output_key="translated",
+    )
 
     # classifying_prompt_template = """
     # Determine if the following content is related to {topic}. If it is related, return "Y"; otherwise, return "N":
@@ -40,7 +46,11 @@ class Assistant:
     ---
     {content}
     """
-    classifying_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(classifying_prompt_template), output_key="relevance")
+    classifying_chain = LLMChain(
+        llm=llm,
+        prompt=PromptTemplate.from_template(classifying_prompt_template),
+        output_key="relevance",
+    )
 
     sentence_analyzing_prompt_template = """
     analyze the following Japanese sentence and using Chinese to explain the used grammar and words:
@@ -48,7 +58,11 @@ class Assistant:
     ---
     {content}
     """
-    sentence_analyzing_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(sentence_analyzing_prompt_template), output_key="analysis")
+    sentence_analyzing_chain = LLMChain(
+        llm=llm,
+        prompt=PromptTemplate.from_template(sentence_analyzing_prompt_template),
+        output_key="analysis",
+    )
 
     question_answering_prompt_template = """
     analyze the following Japanese question and tell me which option is best for chosen answer and explain the reason using Chinese:
@@ -58,38 +72,50 @@ class Assistant:
     ---
     {content}
     """
-    question_answering_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(question_answering_prompt_template), output_key="answer")
+    question_answering_chain = LLMChain(
+        llm=llm,
+        prompt=PromptTemplate.from_template(question_answering_prompt_template),
+        output_key="answer",
+    )
 
     @classmethod
     def translate(cls, content: str, target_language: str):
-        result: str = cls.translating_chain.run({
-            "content": content,
-            "target_language": target_language.capitalize(),
-        })
+        result: str = cls.translating_chain.run(
+            {
+                "content": content,
+                "target_language": target_language.capitalize(),
+            }
+        )
 
         return result
 
     @classmethod
     def analyze_sentence(cls, content: str):
-        result: str = cls.sentence_analyzing_chain.run({
-            "content": content,
-        })
+        result: str = cls.sentence_analyzing_chain.run(
+            {
+                "content": content,
+            }
+        )
 
         return result
 
     @classmethod
     def answer_question(cls, content: str):
-        result: str = cls.question_answering_chain.run({
-            "content": content,
-        })
+        result: str = cls.question_answering_chain.run(
+            {
+                "content": content,
+            }
+        )
         return result
 
     @classmethod
     def judge_relevance(cls, content, topic="japanese learning"):
-        result: str = cls.classifying_prompt_template.run({
-            "content": content,
-            "topic": topic,
-        })
+        result: str = cls.classifying_prompt_template.run(
+            {
+                "content": content,
+                "topic": topic,
+            }
+        )
 
         return result == "Y"
 
