79b348fe6437571c54eb18db7343c9c9480d7c70
zhaobin <zhaobin_zhang@intsig.net>
2024-08-01 14:04:40 +0800

    feat: 增加上传sku文件的接口

--- tool_blueprint.py
+++ tool_blueprint.py
@@ -3,7 +3,7 @@ import time
 from collections import defaultdict
 
 import pandas as pd
-from flask import Blueprint, jsonify
+from flask import Blueprint, Flask, jsonify, request
 
 from tools import update_product_sku
 
@@ -18,6 +18,23 @@ def lock_and_sync():
 
 task_running = False
 start_time = time.perf_counter() - 600
+
+
+@tool_blueprint.route("/upload_sku", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return "No file part", 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return "No selected file", 400
+
+    if file:
+        file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "tools", "all_sku.xlsx"
+        )
+        file.save(file_path)
+        return "File uploaded successfully", 200
 
 
 @tool_blueprint.route("/sync_sku", methods=["GET"])
@@ -53,9 +70,13 @@ def sync_sku():
         )
         for index in df.index:
             detail = df.loc[index].to_dict()
-            no_matched[detail["my_name"]].append(tuple(str(detail[field]) for field in target_fields))
+            no_matched[detail["my_name"]].append(
+                tuple(str(detail[field]) for field in target_fields)
+            )
 
-        return jsonify({"running": False, "message": log_content, "no_matched": dict(no_matched)})
+        return jsonify(
+            {"running": False, "message": log_content, "no_matched": dict(no_matched)}
+        )
 
     if run_flag == "True" and time.perf_counter() - start_time < 600:
         with open(update_product_sku.LOG_FILE, "r", encoding="utf-8") as log:
