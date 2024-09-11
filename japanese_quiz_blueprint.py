d6bbe258b26c109cce240c4814b04b09ff126ae2
zhaobin <zhaobin_zhang@intsig.net>
2024-09-12 05:42:28 +0800

    feat: /quiz/sort/random接口返回数据切换为从文件读取全量数据

--- japanese_quiz_blueprint.py
+++ japanese_quiz_blueprint.py
@@ -16,7 +16,7 @@ def question_selection():
         return jsonify(choice(quiz_cache["selection_questions"]))
 
     with open(
-        os.path.join(current_folder, "japanese_quiz", "all_select_questions.json"),
+        os.path.join(current_folder, "japanese_quiz", "all_selection_questions.json"),
         "r",
         encoding="utf-8",
     ) as fp:
@@ -28,31 +28,17 @@ def question_selection():
 
 @japanese_quiz_blueprint.route("/sort/random", methods=["GET"])
 def question_sort():
-    questions = [
-        {
-            "question": "9月も半ばで、やっと ________ ________ ________ ________ 。",
-            "options": [
-                "1 涼しくなった",
-                "2 ぶり返した",
-                "3 また残暑が",
-                "4 と思いきゃ",
-            ],
-            "answer": "1432",
-            "explain": {
-                "translation": "【译文】九月都过了一半了,原以为终于要凉快了,没想到又热了起来。",
-                "analysis": "【解析】选项3「また残暑が」与选项2「ぶり返した」构成主谓短语。选项1置于选项4之前,构成「と思いきゃ」的句型,表示“原以为……（却……）。”",
-            },
-        },
-        {
-            "question": "面接のとき、 ________ ________ ________ ________ 困る。",
-            "options": ["1 ようでは", "2 話もできない", "3 緊張の", "4 あまり"],
-            "answer": "3421",
-            "explain": {
-                "translation": "【译文】面试时,如果由于过于紧张而说不出话来的话就不好办了。",
-                "analysis": "【解析】选项1「〜ようでは」表示“如果……的话”,后项常用「困る/だめだ」等负面的表达方式,因此选项1置于最后一个空。选项4「動辞書形/名一の+あまり」表示由于程度过甚,导致了某种结果,因此选项3置于选项4之前。选项2「話もできない」表示“紧张的结果”,置于选项4之后。",
-            },
-        },
-    ]
+    if quiz_cache.get("sort_questions"):
+        return jsonify(choice(quiz_cache["sort_questions"]))
+
+    with open(
+        os.path.join(current_folder, "japanese_quiz", "all_sort_questions.json"),
+        "r",
+        encoding="utf-8",
+    ) as fp:
+        questions = [json.loads(line) for line in fp.readlines()]
+
+    quiz_cache["sort_questions"] = questions
     return jsonify(choice(questions))
 
 
